e3eaf4b5a0401c2b84127790952a1ee8d1728e08
Кузнецов Алексей Эдуардович <akuznetsov@mpl.lc>
2021-10-03 16:15:27 +0300

    Added work with config files

--- src/main/kotlin/com/github/kadehar/Main.kt
+++ src/main/kotlin/com/github/kadehar/Main.kt
@@ -1,12 +1,14 @@
 package com.github.kadehar
 
 import com.github.kadehar.utils.msToTime
+import com.github.kadehar.utils.parseConfig
 import com.github.kadehar.utils.toSummary
 
 fun main() {
-    val file = System.getProperty("file")
-    val summary = toSummary(file)
+    val config = parseConfig()
+    val summary = toSummary(config.project.file)
     val time = msToTime(summary.time.duration)
 
     println(time)
+    println(config.project.language)
 }
--- src/main/kotlin/com/github/kadehar/utils/Util.kt
+++ src/main/kotlin/com/github/kadehar/utils/Util.kt
@@ -1,7 +1,9 @@
 package com.github.kadehar.utils
 
+import com.github.kadehar.config.Config
 import com.github.kadehar.model.Summary
 import com.google.gson.GsonBuilder
+import com.uchuhimo.konf.toValue
 import java.io.FileReader
 import java.text.SimpleDateFormat
 import java.util.*
@@ -15,4 +17,9 @@ fun msToTime(duration: Long): String {
     val formatter = SimpleDateFormat("HH:mm:ss")
     formatter.timeZone = TimeZone.getTimeZone("UTC")
     return formatter.format(Date(duration))
+}
+
+fun parseConfig(): Config {
+    val config = System.getProperty("config")
+    return com.uchuhimo.konf.Config().from.json.file(config).toValue()
 }
