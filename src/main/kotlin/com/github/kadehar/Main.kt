fde9960f9b558838e464fd535bd5b5456c3b1de2
Кузнецов Алексей Эдуардович <akuznetsov@mpl.lc>
2021-10-03 18:40:28 +0300

    Added coroutines to project

--- src/main/kotlin/com/github/kadehar/Main.kt
+++ src/main/kotlin/com/github/kadehar/Main.kt
@@ -1,17 +1,34 @@
 package com.github.kadehar
 
+import com.github.kadehar.async.collectDataAsync
+import com.github.kadehar.async.collectPhrasesAsync
+import com.github.kadehar.async.parseConfigAsync
+import com.github.kadehar.async.parseSummaryAsync
 import com.github.kadehar.client.TelegramApi
+import com.github.kadehar.config.Config
+import com.github.kadehar.model.Summary
 import com.github.kadehar.template.Message
 import com.github.kadehar.utils.collectTemplateData
-import com.github.kadehar.utils.parseConfig
-import com.github.kadehar.utils.toSummary
+import kotlinx.coroutines.runBlocking
 import retrofit2.Call
 import retrofit2.Response
 
 fun main() {
-    val config = parseConfig()
-    val summary = toSummary(config.project.file)
-    val map = collectTemplateData(config.project, summary)
+    var config: Config
+    runBlocking {
+        config = parseConfigAsync()
+    }
+
+    var summary: Summary
+    var phrases: Map<String, String>
+    var data: Map<String, Any>
+    runBlocking {
+        summary = parseSummaryAsync(config.project.file)
+        phrases = collectPhrasesAsync(config.project.language)
+        data = collectDataAsync(config.project, summary)
+    }
+
+    val map = collectTemplateData(phrases, data)
     val text = Message().createMessage(map)
     val client = TelegramApi.create().sendMessage(
         token = config.bot.token,
